cf8cb355bf31859ec1b7ae052942087c848ce62e
Tanvir225 <tanvirrehman200@gmail.com>
2026-01-15 01:24:14 +0600

    server api added

--- src/Hook/usePublicAxios.jsx
+++ src/Hook/usePublicAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/', // Base URL for the API
+    baseURL: 'https://food-fair-backend.vercel.app/api/', // Base URL for the API
     withCredentials: true,
     
 });
